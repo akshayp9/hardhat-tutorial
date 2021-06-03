4c38f5508da04c05b9734ff153bc597b4d863256
AksNode <akshayp67@gmail.com>
2021-06-03 19:07:10 +0530

    ropsten n/w added'

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,6 +3,21 @@
  */
 
 require("@nomiclabs/hardhat-waffle");
+require("hardhat-gas-reporter");
+
+const ROPSTEN_PRIVATE_KEY = "YOUR ROPSTEN PRIVATE KEY";
+
+
 module.exports = {
   solidity: "0.7.3",
+  networks: {
+    ropsten: {
+      url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`0x${ROPSTEN_PRIVATE_KEY}`]
+    }
+  },
+  gasReporter: {
+    currency: 'USD',
+    gasPrice: 21
+  }
 };
